03291ccae28c61a020af2df702ee6be9866bbfd8
Vanaja Sangati <vanaja1073@gmail.com>
2023-07-03 21:52:11 +0530

    services

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 
 import { Navbar,Nav,Container } from "react-bootstrap"
-
+import Services from "./Services";
 
 import "./Header.css";
 import imgLogo from "./../images/logo.jpeg";
@@ -8,20 +8,21 @@ import imgLogo from "./../images/logo.jpeg";
 
 const Header=(props)=>{
     return(
-        <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">
-              <img class="logo" src={imgLogo} alt="logo"/>
-          </Navbar.Brand>
-          <Nav className="ml-auto">
-            <Nav.Link  className="nav-link" href="#home">Home</Nav.Link>
-            <Nav.Link  className="nav-link" href="#aboutus">About us</Nav.Link>
-            <Nav.Link  className="nav-link" href="#contactus" onClick={()=> { props.setShowForm(true);}}>Contact us</Nav.Link>
-            <Nav.Link  className="nav-link" href="#careers" >Careers</Nav.Link>
-            <Nav.Link  className="nav-link" href="#services">Services</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+        
+      <><Navbar bg="dark" data-bs-theme="dark" >
+      <Container>
+        <Navbar.Brand href="#home">
+            <img class="logo" src={imgLogo} alt="logo"/>
+        </Navbar.Brand>
+        <Nav className="ml-auto">
+          <Nav.Link  className="nav-link" href="#home">Home</Nav.Link>
+          <Nav.Link  className="nav-link" href="#aboutus">About us</Nav.Link>
+          <Nav.Link  className="nav-link" href="#contactus" onClick={()=> { props.setShowForm(true);}}>Contact us</Nav.Link>
+          <Nav.Link  className="nav-link" href="#careers" >Careers</Nav.Link>
+          <Nav.Link  className="nav-link" href="#services">Services</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar><div><Services/></div></>
     )
     
 }
---
+++ src/components/ServiceItem.js
@@ -0,0 +1,34 @@
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
+import "./Services.css"
+
+const ServiceItem=(props)=>{
+  const {serviceDetails}=props
+  const {serviceImg,serviceName,description}=serviceDetails
+  console.log(props)
+    return(<>
+    {/* <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={serviceImg} className='service-img'/>
+    <Card.Body>
+      <Card.Title>{serviceName}</Card.Title>
+      <Card.Text>
+        {description}
+      </Card.Text>
+      <Button variant="primary">Go somewhere</Button>
+    </Card.Body>
+  </Card> */}
+  <div className="services-container">
+    <div className="service-card">
+    <div ><img className="service-img" src={serviceImg} alt="service"/></div>
+    <div><h1 className="heading">{serviceName}</h1><p>{description}</p>
+    <button className="know-more-btn" type="submit">KNOW MORE</button></div>
+    </div>
+   
+  </div>
+
+  </>)
+  
+
+
+}
+export default ServiceItem
